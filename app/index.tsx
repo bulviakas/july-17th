ddfaae66e435fc1e6b1d92ca6b8a7c0e0f92e590
bulviakas <misteridiot2004@gmail.com>
2026-07-04 17:40:24 +0300

    Now we have an empty home page and about tab

---
+++ app/index.tsx
@@ -0,0 +1,27 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from 'expo-router'
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+      <Link href="/about" style={styles.button}>Go to About screen</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2e2825',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    color: 'purple'
+  }
+});
