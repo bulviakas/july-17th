77449e1f8f692eb5e06408c451151ca9e69c3585
bulviakas <misteridiot2004@gmail.com>
2026-07-16 01:26:41 +0300

    Made the submitted images be shown on the photo film roll

--- components/Filmframe.tsx
+++ components/Filmframe.tsx
@@ -2,6 +2,7 @@ import { PhotoChallenge } from '@/types/PhotoChallenge';
 import React from 'react';
 import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import { useChallenges } from '../contexts/ChallengeProvider';
+import { Image } from 'expo-image';
 
 interface FilmFrameProps {
   challenge: PhotoChallenge;
@@ -9,17 +10,15 @@ interface FilmFrameProps {
 }
 
 export default function FilmFrame({ challenge, onPress }: FilmFrameProps) {
-  const { isCompleted } = useChallenges();
+  const { isCompleted, getChallengePhoto } = useChallenges();
   const done = isCompleted(challenge.id);
+  const photoUri = getChallengePhoto(challenge.id);
 
   return (
-    <TouchableOpacity
-      style={styles.frameOuter}
-      onPress={() => onPress(challenge)}
-      activeOpacity={0.8}
-    >
+    <TouchableOpacity style={styles.frameOuter} onPress={() => onPress(challenge)} activeOpacity={0.8} >
       <View style={[styles.frameInner, done && styles.frameInnerDone]}>
-        {/* */}
+        {photoUri && (
+          <Image source={{ uri: photoUri }} style={styles.frameImage} contentFit="cover" />)}
       </View>
     </TouchableOpacity>
   );
@@ -40,8 +39,13 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderRadius: 6,
     borderColor: '#ae1368',
+    overflow: "hidden",
   },
   frameInnerDone: {
     borderColor: '#3F184D',
+  },
+  frameImage: {
+    width: '100%',
+    height: '100%',
   },
 });
