7c9e9f20adbc9ec33ea2f16dfa12e758154b91b6
bulviakas <misteridiot2004@gmail.com>
2026-07-16 10:04:43 +0300

    Little tweak in the PhotoChallenge structure

--- types/PhotoChallenge.ts
+++ types/PhotoChallenge.ts
@@ -2,6 +2,6 @@ import { ImageSourcePropType } from 'react-native';
 import { BaseChallenge } from './BaseChallenge';
  
 export interface PhotoChallenge extends BaseChallenge {
-  exampleImage: ImageSourcePropType;
+  exampleImage: ImageSourcePropType | string;
   creditText: string;
 }
